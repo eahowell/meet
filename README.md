fa906946ef547e6c4f31098f56b0aa9aab6f96fa
Liz <51245710+eahowell@users.noreply.github.com>
2024-09-19 15:54:07 -0500

    Update manifest and icons

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![CliqueUPLogo](public/LightLogo.png)
+![CliqueUPLogo](public/LightLogo.webp)
 
 # Welcome to CliqueUP!
 
@@ -62,7 +62,7 @@ Scenario: User should see a list of suggestions when they search for a city
 Scenario: User can select a city from the suggested list
   Given the user was typing “Berlin” in the city textbox AND the list of suggested cities is showing
   When the user selects a city (e.g., “Berlin, Germany”) from the list
-  Then their city should be changed to that city (i.e., “Berlin, Germany”) AND the user should 
+  Then their city should be changed to that city (i.e., “Berlin, Germany”) AND the user should
   receive a list of upcoming events in that city
 
 Scenario: User filters for a city with no events
--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import { getEvents, extractLocations } from "./api";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Navbar, Container } from "react-bootstrap";
-import BrandImage from "./img/LightLogo.png";
+import BrandImage from "./img/LightLogo.webp";
 import Spinner from "./components/Spinner";
 import { InfoAlert, ErrorAlert } from "./components/Alert";
-import { ThemeProvider } from './contexts/ThemeContext';
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 const EventList = lazy(() => import("./components/EventList"));
 const CitySearch = lazy(() => import("./components/CitySearch"));
@@ -48,48 +48,48 @@ const App = () => {
 
   return (
     <ThemeProvider>
-    <div className="App">
-      {isLoading && <Spinner />}
-      <Navbar>
-        <Container fluid className="d-flex flex-column align-items-center">
-          <Navbar.Brand href="#" className="mb-3">
-            <img
-              id="HeaderLogoImage"
-              src={BrandImage}
-              width="350"
-              height="50"
-              alt="Logo for CliqueUP"
-              className="page-header__item img-fluid"
-            />
-          </Navbar.Brand>
-          <div className="alerts-container">
-            {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
-            {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
-          </div>
-          <Suspense fallback={<Spinner />}>
-            <CitySearch
-              allLocations={allLocations}
-              setCurrentCity={setCurrentCity}
-              setInfoAlert={setInfoAlert}
-            />
-            <br />
-            
-            <NumberOfEvents
-              setCurrentNOE={setCurrentNOE}
-              setErrorAlert={setErrorAlert}
-            />
-            <br />
-            <EventList events={events} />
-          </Suspense>
-          {!isLoading && events.length === 0 && (
-            <p className="noEventsText">
-              No events found. Try refreshing the data and ensure you've
-              connected your gmail account.
-            </p>
-          )}
-        </Container>
-      </Navbar>
-    </div>
+      <div className="App">
+        {isLoading && <Spinner />}
+        <Navbar>
+          <Container fluid className="d-flex flex-column align-items-center">
+            <Navbar.Brand href="#" className="mb-3">
+              <img
+                id="HeaderLogoImage"
+                src={BrandImage}
+                width="350"
+                height="50"
+                alt="Logo for CliqueUP"
+                className="page-header__item img-fluid"
+              />
+            </Navbar.Brand>
+            <div className="alerts-container">
+              {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+              {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+            </div>
+            <Suspense fallback={<Spinner />}>
+              <CitySearch
+                allLocations={allLocations}
+                setCurrentCity={setCurrentCity}
+                setInfoAlert={setInfoAlert}
+              />
+              <br />
+
+              <NumberOfEvents
+                setCurrentNOE={setCurrentNOE}
+                setErrorAlert={setErrorAlert}
+              />
+              <br />
+              <EventList events={events} />
+            </Suspense>
+            {!isLoading && events.length === 0 && (
+              <p className="noEventsText">
+                No events found. Try refreshing the data and ensure you've
+                connected your gmail account.
+              </p>
+            )}
+          </Container>
+        </Navbar>
+      </div>
     </ThemeProvider>
   );
 };
