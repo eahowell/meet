c80e4e267b7e0ebb4bd670d557e25908e6ad93d3
Liz <51245710+eahowell@users.noreply.github.com>
2024-09-23 12:22:33 -0500

    Adjust Style for Dark Mode

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -10,10 +10,10 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
-import { ThemeContext } from '../contexts/ThemeContext';
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const CityEventsChart = ({ events, allLocations }) => {
-    const { isDarkMode } = useContext(ThemeContext);
+  const { isDarkMode } = useContext(ThemeContext);
   const [data, setData] = useState([]);
 
   const getData = useMemo(() => {
@@ -37,7 +37,7 @@ const CityEventsChart = ({ events, allLocations }) => {
     <ResponsiveContainer width="99%" height={400}>
       <ScatterChart
         style={{
-          backgroundColor: isDarkMode ? "#FFEEE6 ":  "#FFEEE6",
+          backgroundColor: isDarkMode ? "#4A6987" : "#FFEEE6",
         }}
         margin={{
           top: 20,
@@ -46,7 +46,7 @@ const CityEventsChart = ({ events, allLocations }) => {
           left: 0,
         }}
       >
-        <CartesianGrid />
+        <CartesianGrid stroke={isDarkMode ? "#495670" : "#ccc"} />
         <XAxis
           type="category"
           dataKey="city"
@@ -55,15 +55,28 @@ const CityEventsChart = ({ events, allLocations }) => {
           interval={0}
           tick={{ dy: 2, textAnchor: "start", transform: "translate(10, 0)" }}
           tickFormatter={(value) => value.split(" ")[0]}
+          stroke={isDarkMode ? "#ECF0F1" : "#333"}
         />
         <YAxis
           type="number"
           dataKey="count"
           name="Number of Events"
           allowDecimals={false}
+          stroke={isDarkMode ? "#ECF0F1" : "#333"}
         />
-        <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-        <Scatter name="Events by City" data={data} fill="#0F4BB8" />
+        <Tooltip 
+          cursor={{ strokeDasharray: "3 3" }} 
+          contentStyle={{
+            backgroundColor: isDarkMode ? "#FFEEE6" : "#fff",
+            color: isDarkMode ? "#ECF0F1" : "#333",
+            border: `1px solid ${isDarkMode ? "#ECF0F1" : "#ccc"}`,
+          }}
+        />
+        <Scatter 
+          name="Events by City" 
+          data={data} 
+          fill={isDarkMode ? "#ECF0F1" : "#0F4BB8"} 
+        />
       </ScatterChart>
     </ResponsiveContainer>
   );
